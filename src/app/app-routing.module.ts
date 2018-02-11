7fe31f94a63419415300860c197df6eb5c8b5d7d
Kluckmuck <kluckmucki@gmail.com>
2018-02-11 15:14:56 +0000

    Created scrollable panel with filler text. Accept button. Perhaps implement requirement to scroll through the whole panel. Changed header font, cleaner and slimmer look now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShirtComponent } from './shirt/shirt.component';
 import { ShirtDetailComponent } from './shirtdetail/shirtdetail.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { TermsComponent } from './terms/terms.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'shirts', component: ShirtComponent },
   { path: 'item/:id', component: ShirtDetailComponent },
-  { path: 'checkout', component: CheckoutComponent }
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'terms', component: TermsComponent }
 ];
 
 @NgModule({
