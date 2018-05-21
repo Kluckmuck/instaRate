4e92885b428c6e4ca41ab2fbba8f5d35d92dcbe7
Kluckmuck <kluckmucki@gmail.com>
2018-05-21 13:08:11 +0200

    removed env

--- instarate-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\program files\git\projects\instarate\instarate-env\scripts\python3.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
